0f8b66420f783c2ec090bfd500e846155c6564f2
achieuw <rasmus.moller@se.experis.com>
2022-02-03 14:39:29 +0100

    FEAT: Add trainer and catalogue page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrainerPageComponent } from './trainer-page/trainer-page.component';
+import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerPageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule
