8d27e347255be81dc60eaf1c3f4244345a5dd895
Aldin Drobic <aldin.drobic@se.experis.com>
2022-02-03 14:56:21 +0100

    CHORE: Declared landing-page in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TrainerPageComponent } from './trainer-page/trainer-page.component';
 import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrainerPageComponent,
-    CataloguePageComponent
+    CataloguePageComponent, 
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
