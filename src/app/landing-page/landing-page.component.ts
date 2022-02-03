d8dc83ffba06af05e81f1a13de866321ea86e767
Aldin Drobic <aldin.drobic@se.experis.com>
2022-02-03 16:25:54 +0100

    FIX: Fixed code regarding fetch trainer data

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainesService } from '../services/trainers.service';
 
 @Component({
-selector: 'app-landing-page',
-templateUrl: './landing-page.component.html',
-styleUrls: ['./landing-page.component.css'],
+    selector: 'app-landing-page',
+    templateUrl: './landing-page.component.html',
+    styleUrls: ['./landing-page.component.css'],
 })
 
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+    //Using trainers service to fetch data
+    constructor(private readonly trainersService: TrainesService) {
 
+    }
 
+    ngOnInit(): void {
+        this.trainersService.fetchTrainers();
+    }
 
 }
--- src/app/services/trainers.service.ts
+++ src/app/services/trainers.service.ts
@@ -1,20 +1,25 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Trainer } from "../models/trainer.model";
 
 @Injectable({
     providedIn: 'root'
 })
-export class UsersService {
-    private trainers = []
+//Service for fetching trainers
+export class TrainesService {
 
+    private trainers: Trainer[] = [];//Using Trainer model to store fetched trainar data
+    private error: string = '';
     constructor(private readonly http: HttpClient) {
     }
      
     public fetchTrainers(): void {
-        this.http.get('https://heroku-test-api-rasmus.herokuapp.com/trainers')
-        .subscribe(trainer => {
-            this.trainer = trainer
-        })
+        this.http.get<Trainer[]>('https://heroku-test-api-rasmus.herokuapp.com/trainers')
+        .subscribe((trainers: Trainer[]) => {
+            this.trainers = trainers;
+        }, (error: HttpErrorResponse) => {
+            this.error = error.message;
+        }); 
     }
 
 }
