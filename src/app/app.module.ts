68e381341ccb363d9afe854164563e3ee5f06480
achieuw <rasmus.moller@se.experis.com>
2022-02-03 16:45:50 +0100

    BUILD: http requests (wip)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { TrainerPageComponent } from './trainer-page/trainer-page.component';
 import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { ErrorMessageComponent } from './error-message/error-message.component';
 
 
 @NgModule({
@@ -13,11 +16,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     AppComponent,
     TrainerPageComponent,
     CataloguePageComponent, 
-    LandingPageComponent
+    LandingPageComponent,
+    PokemonDetailComponent,
+    ErrorMessageComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-pokemans.ts
@@ -0,0 +1,14 @@
+import { Pokemon } from "./pokemon"
+
+export const POKEMONS: Pokemon[] = [
+  { id: 11, name: "Pikachu" },
+  { id: 12, name: "Pimpkachu" },
+  { id: 13, name: "Pikapee" },
+  { id: 14, name: "Pikapoo" },
+  { id: 15, name: "Pookaep" },
+  { id: 16, name: "Keepo" },
+  { id: 17, name: "Pekkachu" },
+  { id: 18, name: "Pokichu" },
+  { id: 19, name: "Pokeman" },
+  { id: 20, name: "Pankake" },
+];
