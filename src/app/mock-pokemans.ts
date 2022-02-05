45a5f450f2aa04199bbd73d5190d8268f138125b
achieuw <rasmus.moller@se.experis.com>
2022-02-05 12:04:21 +0100

    BUILD: update pokemons model

--- src/app/mock-pokemans.ts
+++ src/app/mock-pokemans.ts
@@ -1,14 +1,14 @@
 import { Pokemon } from "./models/pokemon.model"
 
 export const POKEMONS: Pokemon[] = [
-  { id: 11, name: "Pikachu", image: ""},
-  { id: 12, name: "Pimpkachu", image: "" },
-  { id: 13, name: "Pikapee", image: "" },
-  { id: 14, name: "Pikapoo", image: "" },
-  { id: 15, name: "Pookaep", image: "" },
-  { id: 16, name: "Keepo", image: "" },
-  { id: 17, name: "Pekkachu", image: "" },
-  { id: 18, name: "Pokichu", image: "" },
-  { id: 19, name: "Pokeman", image: "" },
-  { id: 20, name: "Pankake", image: "" },
+  { id: 11, name: "Pikachu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/333.png", collected: true},
+  { id: 12, name: "Pimpkachu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/498.png", collected: true },
+  { id: 13, name: "Pikapee", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png", collected: true},
+  { id: 14, name: "Pikapoo", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/310.png", collected: true},
+  { id: 15, name: "Pookaep", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/98.png", collected: true },
+  { id: 16, name: "Keepo", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/42.png", collected: true },
+  { id: 17, name: "Pekkachu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/237.png", collected: true },
+  { id: 18, name: "Pokichu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/137.png", collected: true },
+  { id: 19, name: "Chuchutrainpekman", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/32.png", collected: true },
+  { id: 20, name: "Pankakechu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/37.png", collected: false },
 ];
