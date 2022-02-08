e4bd1e36026a608daa97f5ba738abae3c19170dc
achieuw <rasmus.moller@se.experis.com>
2022-02-08 16:33:32 +0100

    FIX: curly bracket add

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ListItemDecorator } from '../models/list-item-decorator.model';
 import { Pokemon } from '../models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnChanges {
   @Input() pokemonsList: Pokemon[];
-  @Input() decoratorType: "Trainer" | "Catalogue";
+  @Input() decoratorType: 'Trainer' | 'Catalogue';
 
   //List of all Pokemon combined with decorator type to determine which decorator component to render together with the Pokemon List Items
   pokemons: ListItemDecorator[];
 
-  constructor() { 
+  constructor() {
     this.pokemonsList = [];
     this.pokemons = [];
-    this.decoratorType = "Catalogue"
+    this.decoratorType = 'Catalogue';
   }
   ngOnChanges(changes: SimpleChanges): void {
-      this.pokemons = []
-      for(let pokemon of this.pokemonsList){
-        this.pokemons.push({"pokemon": pokemon, "decoratorType":this.decoratorType})
+    this.pokemons = [];
+    for (let pokemon of this.pokemonsList) {
+      this.pokemons.push({
+        pokemon: pokemon,
+        decoratorType: this.decoratorType,
+      });
+    }
   }
 }
